2231f434cd837694d738280c641a248b3a9377b0
Benjamin Zhuang <v-bzhuang@microsoft.com>
2023-10-23 12:34:43 -0700

    removed generateMetadata due to some bugs

--- src/app/problems/[id]/layout.tsx
+++ src/app/problems/[id]/layout.tsx
@@ -1,27 +1,25 @@
-import type { Metadata, ResolvingMetadata } from "next";
+// type Props = {
+//   params: { id: string };
+//   searchParams: { [key: string]: string | string[] | undefined };
+// };
 
-type Props = {
-  params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-};
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata,
+// ): Promise<Metadata> {
+//   // read route params
+//   const id = params.id;
+//   const problemName = id
+//     .split("-")
+//     .map(
+//       (token) => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase(),
+//     )
+//     .join(" ");
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata,
-): Promise<Metadata> {
-  // read route params
-  const id = params.id;
-  const problemName = id
-    .split("-")
-    .map(
-      (token) => token.charAt(0).toUpperCase() + token.slice(1).toLowerCase(),
-    )
-    .join(" ");
-
-  return {
-    title: `${problemName} | LeetCode Clone`,
-  };
-}
+//   return {
+//     title: `${problemName} | LeetCode Clone`,
+//   };
+// }
 
 export default function ProblemsLayout({
   children,
