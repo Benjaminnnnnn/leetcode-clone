09f4ae4dd3da38c02224bc6f084fde6218e5b42e
benjaminnnnnn <benjaminnnnnnzhuang@gmail.com>
2023-12-25 17:16:16 -0800

    added landing & auth page tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("has title", async ({ page }) => {
+  await page.goto("https://leetcode-clone-indol.vercel.app/");
+  await expect(page).toHaveTitle("LeetCode Clone");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("auth available", async ({ page }) => {
+  await page.goto("https://leetcode-clone-indol.vercel.app/");
+  await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(
+    page.getByRole("button", { name: "Create an Account" }),
+  ).toBeVisible();
 });
